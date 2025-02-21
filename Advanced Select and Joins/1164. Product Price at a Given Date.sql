e287d966e7e52b2de5ef058fa40c5af0e4ecdf06
madhujammm <155441219+madhujammm@users.noreply.github.com>
2025-02-21 10:07:01 +0530

    Update 1164. Product Price at a Given Date.sql

--- Advanced Select and Joins/1164. Product Price at a Given Date.sql
+++ Advanced Select and Joins/1164. Product Price at a Given Date.sql
@@ -1,11 +1,9 @@
-WITH latest_price AS (
-    SELECT product_id, new_price, change_date
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-)
 SELECT p.product_id, 
-       COALESCE(MAX(lp.new_price), 10) AS price
-FROM (SELECT DISTINCT product_id FROM Products) p
-LEFT JOIN latest_price lp 
-ON p.product_id = lp.product_id
-GROUP BY p.product_id;
+       COALESCE((
+           SELECT new_price 
+           FROM Products p2
+           WHERE p2.product_id = p.product_id AND p2.change_date <= '2019-08-16'
+           ORDER BY p2.change_date DESC
+           LIMIT 1
+       ), 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) p;
