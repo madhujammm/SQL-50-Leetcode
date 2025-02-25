3d23d995de3c155e326cbfda0c658210cfadc0e2
madhujammm <155441219+madhujammm@users.noreply.github.com>
2025-02-25 10:55:35 +0530

    Update 602. Friend Requests II: Who Has the Most Friends.sql

--- Subqueries/602. Friend Requests II: Who Has the Most Friends.sql
+++ Subqueries/602. Friend Requests II: Who Has the Most Friends.sql
@@ -1,1 +1,19 @@
+WITH FriendCounts AS (
+    SELECT requester_id AS id, COUNT(accepter_id) AS num
+    FROM RequestAccepted
+    GROUP BY requester_id
+    UNION ALL
+    SELECT accepter_id AS id, COUNT(requester_id) AS num
+    FROM RequestAccepted
+    GROUP BY accepter_id
+),
+AggregatedCounts AS (
+    SELECT id, SUM(num) AS total_friends
+    FROM FriendCounts
+    GROUP BY id
+)
+SELECT id, total_friends AS num
+FROM AggregatedCounts
+ORDER BY num DESC
+LIMIT 1;
 
